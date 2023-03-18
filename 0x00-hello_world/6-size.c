64bf98abe5cbaa35fa32f45d29ade89666f90f90
Matthew Oluwabusayo Opoola <initoluwa@gmail.com>
2023-03-17 19:14:23 -0700

    sizeOf

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,8 +8,8 @@ int main(void)
 {
 	printf("Size of a char: %d byte(s)\n", sizeOf(char));
 	printf("Size of an int: %d byte(s)\n", sizeOf(int));
-	printf("Size of a long int: %d byte(s)\n" sizeOf(long int));
-	printf("Size of a long long int: %d byte(s)\n" sizeOf(long long int));
-	printf("Size of a float: %d byte(s)\n" sizeOf(float));
+	printf("Size of a long int: %d byte(s)\n", sizeOf(long int));
+	printf("Size of a long long int: %d byte(s)\n", sizeOf(long long int));
+	printf("Size of a float: %d byte(s)\n", sizeOf(float));
 	return(0);
 }
