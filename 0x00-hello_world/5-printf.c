ba4ac8a133ea7aab72bed9924e2766c030b2a3f3
Matthew Oluwabusayo Opoola <initoluwa@gmail.com>
2023-03-17 18:47:32 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+/**
+ * main - prints out the phrase"with proper grammer, but the outcome is a pieace of art"
+ * return - 0 if success and something else in !success
+ */
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a pieace of art,\n");
+	return (0);
+}
