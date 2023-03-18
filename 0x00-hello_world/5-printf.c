bb14a205b8b4ca6b3ee71d6bcb59583b78dac5c4
Matthew Oluwabusayo Opoola <initoluwa@gmail.com>
2023-03-17 18:53:58 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,8 @@
 #include<stdio.h>
 
 /**
- * main - prints out the phrase"with proper grammar, but the outcome is a piece of art"
+ * main - prints out the phrase"with proper grammar,
+ * but the outcome is a piece of art"
  * return - 0 if success and something else in !success
  */
 int main(void)
