12ed0150db6658ca92799b6a14140787e2884705
Matthew Oluwabusayo Opoola <initoluwa@gmail.com>
2023-03-24 19:55:30 -0700

    0x02-functions_nested_loops

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * _isalpha - checks for alphabetic character
+ * _isupper - checks for uppercase character
  * @c: the character to be checked
- * Return: 1 if c is a letter, 0 otherwise
+ * Return: 1 if c is uppercase, 0 otherwise
  */
-int _isalpha(int c)
+int _isupper(int c)
 {
-	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
+	return (c >= 'A' && c <= 'Z');
 }
